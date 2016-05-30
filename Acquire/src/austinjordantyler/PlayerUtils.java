e2ad844289020ff9aa438cf17ffd2f05180245af
Tyler Berry <tyler@berryac.com>
2016-05-29 18:18:23 -0600

    some good utilities

---
+++ Acquire/src/austinjordantyler/PlayerUtils.java
@@ -0,0 +1,64 @@
+package austinjordantyler;
+
+import halladay.acquire.ChainType;
+import halladay.acquire.Game;
+import halladay.acquire.Player;
+
+import java.util.List;
+
+public class PlayerUtils {
+    public static boolean willBeMajorityStockHolderWithXMoreShares(Player me,
+                                                                   List<Player> otherPlayers,
+                                                                   ChainType chain,
+                                                                   int xMoreShares) {
+        int numMyCurrentShares = me.getStockSharesCount(chain);
+
+        Player majorityShareHolder = me;
+        int numMajorityShareHolderShares = numMyCurrentShares;
+
+        Player secondShareHolder = me;
+        int numSecondaryShareHolderShares = numMyCurrentShares;
+
+        for (Player otherPlayer : otherPlayers) {
+            int numOtherPlayerShares = otherPlayer.getStockSharesCount(chain);
+            if (numOtherPlayerShares >= numMajorityShareHolderShares) {
+                // shift current majority into secondary
+                secondShareHolder = majorityShareHolder;
+                numSecondaryShareHolderShares = numMajorityShareHolderShares;
+
+                // update majority
+                majorityShareHolder = otherPlayer;
+                numMajorityShareHolderShares = numOtherPlayerShares;
+            }
+        }
+
+        return numMyCurrentShares + xMoreShares >= numSecondaryShareHolderShares;
+    }
+
+    public static boolean willBeMajorityStockHolderAfterPurchasing(Player me,
+                                                                   List<Player> otherPlayers,
+                                                                   ChainType chain) {
+        int numICanBuy = getNumPurchasableSharesInChain(me, chain);
+        return willBeMajorityStockHolderWithXMoreShares(me, otherPlayers, chain, numICanBuy);
+    }
+
+    public static int getNumPurchasableSharesInChain(Player me, ChainType chainType) {
+        int availableShares = chainType.getOutstandingSharesCount();
+        int stockPrice = chainType.getStockPrice(1); // TODO The stock could increase
+        int myCash = me.getCash();
+        int numICanAfford = myCash / stockPrice;
+        return Math.min(numICanAfford, availableShares);
+    }
+
+    public static boolean willBeMajorityStockHolderAfterTrade(Player me,
+                                                              List<Player> otherPlayers,
+                                                              List<ChainType> tradingFrom,
+                                                              ChainType tradingTo) {
+        int numMyShares = 0;
+        for (ChainType defunctChain : tradingFrom) {
+            numMyShares += me.getStockSharesCount(defunctChain);
+        }
+        int numICanTrade = numMyShares / 2;
+        return willBeMajorityStockHolderWithXMoreShares(me, otherPlayers, tradingTo, numICanTrade);
+    }
+}
--- Acquire/src/austinjordantyler/TileUtils.java
+++ Acquire/src/austinjordantyler/TileUtils.java
@@ -1,12 +1,21 @@
 package austinjordantyler;
 
+import halladay.acquire.Chain;
 import halladay.acquire.Game;
 import halladay.acquire.Hotel;
 import halladay.acquire.Location;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TileUtils {
+    public static final double MAX_DISTANCE_FROM_CENTER =
+            Math.sqrt(Math.pow(Game.N_ROWS / 2, 2) + Math.pow(Game.N_COLS / 2, 2));
+
     /**
-     * Returns the distance of a tile from the center of the board where 0.0 is the center
+     * Returns the distance of a tile from the center of the board where 0.0 is the center.
+     * The largest this number can be is equal to MAX_DISTANCE_FROM_CENTER.
      */
     public static double tileDistanceFromCenter(Game game, Hotel tile) {
         Location center = new Location(Game.N_ROWS / 2, Game.N_COLS / 2);
@@ -16,5 +25,25 @@ public class TileUtils {
 
         // a^2 + b^2 = c^2
         return Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+    }
+
+    public static List<Hotel> getTilesInChain(Game game, Chain chain) {
+        List<Hotel> chainTiles = new ArrayList<>(chain.getHotelCount());
+        chainTiles.addAll(game.getPlayedTiles().stream()
+                .filter(chain::contains)
+                .collect(Collectors.toList()));
+        return chainTiles;
+    }
+
+    public static List<Hotel> getConnectingLoneTiles(Game game, Hotel tile) {
+        return getLoneTiles(game).stream()
+                .filter(loneTile -> loneTile.isAdjacent(tile))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Hotel> getLoneTiles(Game game) {
+        return game.getPlayedTiles().stream()
+                .filter(playedTile -> game.getAffiliation(playedTile) == null)
+                .collect(Collectors.toList());
     }
 }
