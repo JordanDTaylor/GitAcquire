c859672a045bbd5dbb64ac34e9debe8629af56da
Tyler Berry <tyler@berryac.com>
2016-05-29 18:53:13 -0600

    modified signature of SmartPlayerFactory

--- Acquire/src/austinjordantyler/SmartPlayerFactory.java
+++ Acquire/src/austinjordantyler/SmartPlayerFactory.java
@@ -10,9 +10,7 @@ public class SmartPlayerFactory implements PlayerFactory {
 
     @Override
     public Player createPlayer(int startingCash){
-        IStrategy a = null;
-        IStrategy b=null;
-        IStrategy c = null;
-        return new SmartPlayer(name, startingCash, a, b, c);
+        IStrategy startingStrategy = new EarlyGameStrategy();
+        return new SmartPlayer(name, startingCash, startingStrategy);
     }
 }
